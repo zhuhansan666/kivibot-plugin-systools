45017c5f3d67c2a99cce04ac0590a26900acfa6f
爱喝牛奶の涛哥 <zhuhansan666@outlook.com>
2023-01-08 17:46:13 +0800

    更新/reboot命令显示

--- index.js
+++ index.js
@@ -31,21 +31,24 @@ async function reboot(event, params, plugin) {
             if (secondCmd == "sys" || secondCmd == "system") {
                 if (os.type() == "Linux" || os.type() == "Darwin") {
                     event.reply(`〓 开始运行 "reboot" 〓`)
+                    const startTime = new Date().getTime()
                     exec("reboot", function(error, stdout, stderr) {
-                        event.reply(`运行 "reboot": ${error}, ${stdout}, ${stderr}`)
+                                event.reply(`〓 运行 "reboot" 〓\n${stdout.length > 0 ? `指令输出: ${stdout}` : ""} ${stderr.length > 0 ? `指令输出: ${stderr}` : ""}共耗时${(new Date().getTime() - startTime) / 1000}秒`)
                     });
                 } else {
                     event.reply(`〓 开始运行 "shutdown /f /r /t 3" 〓`)
+                    const startTime = new Date().getTime()
                     exec("shutdown /f /r /t 3", function(error, stdout, stderr) {
-                        event.reply(`运行 "shutdown /f /r /t 3": ${error}, ${stdout}, ${stderr}`)
+                        event.reply(`〓 运行 "shutdown /f /r /t 3" 〓\n${stdout.length > 0 ? `指令输出: ${stdout}` : ""} ${stderr.length > 0 ? `指令输出: ${stderr}` : ""}共耗时${(new Date().getTime() - startTime) / 1000}秒`)
                     });
                 }
             } else {
                 if (secondCmd == "bot" || secondCmd == "kivi") {
                     // event.reply(`暂不支持`)
                     event.reply(`〓 开始运行 "kivi deploy -f" 〓`)
+                    const startTime = new Date().getTime()
                     exec("kivi deploy -f", function(error, stdout, stderr) {
-                        event.reply(`运行 "kivi deploy -f": ${error}, ${stdout}, ${stderr}`)
+                        event.reply(`〓 运行 "kivi deploy -f" 〓\n${stdout.length > 0 ? `指令输出: ${stdout}` : ""} ${stderr.length > 0 ? `指令输出: ${stderr}` : ""}共耗时${(new Date().getTime() - startTime) / 1000}秒`)
                         process.exit()
                     });
                 } else {
