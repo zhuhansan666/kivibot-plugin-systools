b0f9a10ef48b89684fba3bf5bf26703df0dafe6a
爱喝牛奶の涛哥 <zhuhansan666@outlook.com>
2023-01-08 16:33:36 +0800

    新增 /cmd 命令

--- README.md
+++ README.md
@@ -10,3 +10,13 @@
 `npm install kivibot-plugin-reboot-tools`
 或在qq对机器人发送
 `/p add reboot-tools(等价于/plugin add reboot-tools)`
+
+# 如何使用
+* `/reboot help`  ->  看查/reboot帮助
+* `/reboot sys/system`  ->  重启系统
+* `/reboot bot/kivi`  ->  重启框架
+* `/cmd`  ->  看查/cmd帮助
+* `/cmd <system-command>`  ->  运行系统命令(此处<...>代表命令, 无需在调用时包含)
+
+# Know issues / 已知问题
+* 使用`/cmd`运行长时间交互式界面(如`python`/`ipython`/`node`)不会有返回值, 线程阻塞, 但不影响其他功能, `/cmd`任可正常使用
--- index.js
+++ index.js
@@ -2,29 +2,35 @@ const os = require("node:os")
 const exec = require('child_process').exec;
 const { KiviPlugin, segment, http } = require('@kivibot/core')
 const { kiviConf } = require("@kivibot/core")
+const iconv = require('iconv-lite');
+const encoding = 'cp936';
+const binaryEncoding = 'binary';
 
-// const { version } = require('../package.json')
+const { version } = require('./package.json')
 const plugin = new KiviPlugin('reboot-tools', '0.0.1')
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
+function isAdmin(event, mainOnly = false) {
+    if (mainOnly) {
+        return plugin.admins[0] === event.sender.user_id;
+    } else {
+        return plugin.admins.includes(event.sender.user_id);
+    }
+
 }
 
-async function main(event, params, plugin) {
-    // 是否是管理员
-    const isAdmin = kiviConf.admins.includes(event.sender.user_id);
+async function reboot(event, params, plugin) {
     // 是否是主管理员
-    const isMainAdmin = kiviConf.admins[0] === event.sender.user_id;
+    const isMainAdmin = isAdmin(true);
 
     // console.log(params)
     secondCmd = params[0]
     if (secondCmd == "help") {
-        event.reply(`/reboot sys/system  ->  重启系统\n/reboot bot/kivi  ->  重启框架`)
+        event.reply(`〓 reboot-tools./reboot 帮助 〓\n/reboot sys/system  ->  重启系统\n/reboot bot/kivi  ->  重启框架`)
     } else {
         if (isMainAdmin) {
             if (secondCmd == "sys" || secondCmd == "system") {
                 if (os.type() == "Linux" || os.type() == "Darwin") {
-                    event.reply(`开始运行 "reboot"`)
+                    event.reply(`开始运行 "shutdown /f /r /t 3"`)
                     exec("reboot", function(error, stdout, stderr) {
                         event.reply(`运行 "reboot": ${error}, ${stdout}, ${stderr}`)
                     });
@@ -47,14 +53,36 @@ async function main(event, params, plugin) {
                 }
             }
         } else {
-            event.reply(`Permission Error: 非主管理员`, true)
+            event.reply(`Permission Error: 非主管理员`)
         }
     }
 
 }
 
+async function runCmd(event, params, plugin) {
+    secondCmd = params[0]
+    otherCmds = params.slice(0, params.length)
+    if (secondCmd == undefined) {
+        event.reply(`〓 reboot-tools./cmd帮助 〓\n使用/cmd <system-command>执行系统命令`)
+    } else {
+        if (isAdmin(event, true)) {
+            cmdString = otherCmds.join(` `)
+            event.reply(`开始执行 ${cmdString}`)
+            const startTime = new Date().getTime()
+            exec(cmdString, { encoding: binaryEncoding }, function(error, stdout, stderr) {
+                        stdout = iconv.decode(new Buffer.from(stdout, binaryEncoding), encoding)
+                        stderr = iconv.decode(new Buffer.from(stderr, binaryEncoding), encoding)
+                        event.reply(`〓 运行 "${cmdString}" 〓\n${stdout.length > 0 ? `指令输出: ${stdout}` : ""} ${stderr.length > 0 ? `指令输出: ${stderr}` : ""}共耗时${(new Date().getTime() - startTime) / 1000}秒`)
+            });
+        } else {
+            event.reply(`Permission Error: 非主管理员`)
+        }
+    }
+}
+
 plugin.onMounted(() => {
-    plugin.onCmd('/reboot', (event, params) => main(event, params, plugin))
+    plugin.onCmd('/reboot', (event, params) => reboot(event, params, plugin))
+    plugin.onAdminCmd('/cmd', (event, params) => runCmd(event, params, plugin))
 })
 
 module.exports = { plugin }
