8a6d14a30551318c874894a6ad855b867ed0a03e
爱喝牛奶の涛哥 <zhuhansan666@outlook.com>
2023-01-15 12:46:45 +0800

    再一次修复`/cmd`指令在使用等价指令(如`/c`)时命令字符分割不正确的问题

--- index.js
+++ index.js
@@ -203,7 +203,8 @@ function runCmd(event, params, plugin) {
     if (secondCmd == undefined) {
         event.reply(`〓 systool./cmd帮助 〓\n使用/cmd <system-command>执行系统命令`)
     } else {
-        cmdString = event.raw_message.split(" ", 1)[1]
+        command = event.raw_message.split(" ", 1)[0]
+        cmdString = event.raw_message.slice(command.length - 1, cmdString.length - 1)
         if (isAdmin(event, true)) {
             event.reply(`〓 开始执行 ${cmdString} 〓`)
             const startTime = new Date().getTime()
